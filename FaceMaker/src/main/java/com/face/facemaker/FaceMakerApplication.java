ebde4c51520dbca301d086c55fa86cf7d85407f8
jeonghyunKim <jh353633@gmail.com>
2020-06-19 17:49:03 +0900

    first commit facemaker

---
+++ FaceMaker/src/main/java/com/face/facemaker/FaceMakerApplication.java
@@ -0,0 +1,13 @@
+package com.face.facemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaceMakerApplication.class, args);
+	}
+
+}
---
+++ FaceMaker/src/test/java/com/face/facemaker/FaceMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.face.facemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
